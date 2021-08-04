07b31a9af0d85482089fec7a9ee5c28c7cea1301
Jiyong Sung <jiyong9025@gmail.com>
2021-08-04 15:54:49 +0900

    클래스 연습2

--- class1.py
+++ class1.py
@@ -1,11 +1,16 @@
 # class1.py
 #클래스를 정의
 class Person:
-    def__init__(self):
-    self.name = "default name"
+    def __init__(self):
+       self.name = "default name"
     def print(self):
         print("My name is {0}".format(self.name))
 
 #인스턴스 생성
 p1=Person()
 p1.print()
+p1.name = "전우치"
+
+p1.print()
+p2.print()
+
